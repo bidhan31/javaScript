ece4523e159848f454f39ffcb209dfd97f649cde
bidhan31 <136260062+bidhan31@users.noreply.github.com>
2025-01-10 20:45:07 +0000

    fff

--- dataTypes.js
+++ dataTypes.js
@@ -1,1 +1,60 @@
-sadfa
+ 
+//......Conversion......
+
+ //number = 2 to the power 53
+ //bigint
+ //Strig = ""
+ //boolean = true/false 
+ //null = standaone value
+ //Undifined 
+ //Sambol = unique 
+
+ //object
+
+ let score = "33amhf"
+ console.log(typeof score)
+ console.log(typeof(score))
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber)
+
+ //33 = 33
+ //33abc = NaN
+ //true = 1 , false = 0
+ // null = 0
+ 
+ let a = 1
+ let b = Boolean(a)
+ console.log(b)
+
+
+ let nam = 33
+ let output = String(nam)
+ console.log(output)
+ console.log(typeof output)
+
+ // "" = false
+ //"bormon" = true
+
+
+
+ //.........Operation............
+
+
+ let value = 3
+ let negvalue = -value 
+ console.log(negvalue)
+
+
+ let str1 = "hello"
+ let str2 = " bormon"
+ let str3 = str1 + str2
+ console.log(str3)
+ 
+
+
+
+
+
+
