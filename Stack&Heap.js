d99e63942a46a987778c087835b15e773f5b920c
bidhan31 <136260062+bidhan31@users.noreply.github.com>
2025-01-11 11:01:41 +0000

    uu

--- Stack&Heap.js
+++ Stack&Heap.js
@@ -4,3 +4,15 @@ let  myYoutubename = "Bormon404"
 
 let anothername = myYoutubename
 console.log(anothername)
+
+let userOne = {
+    email: "bormon404@gmail.com"
+    password: "12345"
+}
+
+let userTwo = userOne
+
+let userTwo.email = "bormon404@gmail.com
+console.log (userOne.email )
+console.log(userTwo.email)
+"
