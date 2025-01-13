79a8854ca6816a1e522f85c148acc015972de10c
bidhan31 <136260062+bidhan31@users.noreply.github.com>
2025-01-13 14:19:50 +0600

    hhh

---
+++ Objects/obj1.js
@@ -0,0 +1,13 @@
+// object literals........
+
+
+const JsUser = {
+    name: "Bormon",
+    age: 24,
+    Email: "bormon404@gmail.com",
+    isoLoggedIn : false,
+    lastdayLogin : ["monday", "friday"]
+
+}
+
+console.log(JsUser["name"])
