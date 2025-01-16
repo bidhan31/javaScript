e2e905a23ae3fe64ec233411f042d866ff633055
bidhan31 <bidhanbormon08@gmail.com>
2025-01-16 17:49:23 +0600

    mmm

---
+++ Function/arrow.js
@@ -0,0 +1,41 @@
+
+const user ={
+    username : "bormon",
+    price : 999,
+    welcomeMassage : function(){
+        //console.log(`${this.username}, welcome to website`)
+        //console.log(this)
+
+    }
+}
+//user.welcomeMassage()
+//user.username = "voxel"
+//user.welcomeMassage()
+
+//console.log(this)
+
+//function chai(){
+    //console.log(this)
+
+//}
+//chai()
+//const chai = function(){
+    //let username = "mickey"
+    //console.log(this)
+
+//}
+//chai()
+
+//const addtwo= ( n1, n2) => {
+    //return n1 + n2
+
+//}
+
+const addtwo= ( n1, n2) =>  (n1 + n2)                    //implicit return
+
+
+console.log(addtwo(7, 8))
+
+
+
+
