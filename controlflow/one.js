60cdae5fccaf0f591aa830319a267814f21ddb1d
bidhan31 <bidhanbormon08@gmail.com>
2025-03-03 14:24:46 +0600

    hhh

---
+++ controlflow/one.js
@@ -0,0 +1,30 @@
+// const temp = 51
+
+// if(temp === 41){
+//     console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+// console.log("execute");
+
+
+
+// const bal = 1000
+//  if(bal < 500){
+//     console.log("less than 500");
+//  }else if(bal < 750){
+//     console.log("less than 750");
+//  }else if(bal > 950){
+//     console.log("greater than 950");
+//  }else{
+//     console.log("less than 1200");
+//  }
+
+
+const user = true
+const userlogedin = true
+const loggedinfromgoogle = false
+
+if(user && userlogedin ){
+    console.log("allow to buy cursee");
+}
